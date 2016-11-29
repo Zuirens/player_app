1592fd0707605d4ab83417a1dc51e9c1cd633fb4
Ryan <tripper@TripperdeMacBook-Pro.local>
2016-11-29 17:10:24 +0800

    change list_display

--- admin.py
+++ admin.py
@@ -4,10 +4,30 @@ from .models import Comment, ControlMeta, FbAuthenUser, StreamStatistic
 
 
 class FbAuthenUserAdmin(admin.ModelAdmin):
-    list_display = ('nick_name', 'username', 'pic_thumb', 'is_blacklist')
+    list_display = ('edit_link', 'nick_name', 'username', 'pic_thumb', 'is_blacklist')
+    list_editable = ('is_blacklist',)
+
+    list_display_links = None
+
+    def edit_link(self,obj):
+        return u'<button>%s</button>' % (obj.nick_name)
+    edit_link.allow_tags = True
+    edit_link.short_description = "EDIT"
+
+    # def get_list_display_links(self, request, list_display):
+    #     print('--------------', repr(reduce))
+    #     return list_display
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('uid', 'body', 'recieved_time', 'pub_count', 'is_blacklist')
+    list_display = ('uid', 'get_author', 'body', 'recieved_time', 'pub_count', 'is_blacklist')
+    list_editable = ('is_blacklist',)
+    search_fields = ('author__nick_name',)
+    list_filter = ('author__nick_name', 'body',)
+
+    def get_author(self, obj):
+        return obj.author.nick_name
+    get_author.short_description = 'Author'
+    # get_author.admin_order_field = 'author__nick_name'
 
 
 class ControlMetaAdmin(admin.ModelAdmin):
